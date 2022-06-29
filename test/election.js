34c632dc4025ab7f4969619209e1dfc39c68d30e
wael12de <62562595+wael12de@users.noreply.github.com>
2022-06-29 11:34:05 +0100

    Update election.js

--- test/election.js
+++ test/election.js
@@ -1,3 +1,21 @@
+import React, { Component }         from 'react'
+import PropTypes                    from 'prop-types'
+import { MuiThemeProvider }         from '@material-ui/core/styles'
+import { connect }                  from 'react-redux'
+import { bindActionCreators }       from 'redux'
+import * as providerActionCreators  from 'core/actions/actions-provider'
+import {
+  HashRouter,
+  Route,
+  Redirect,
+  Switch
+} from 'react-router-dom'
+import theme                        from 'configs/theme/config-theme'
+import HomeView                     from 'containers/HomeView'
+import Header                       from './components/Header'
+import Footer                       from './components/Footer'
+
+import './styles.scss' // global styles
 var Election = artifacts.require("./Election.sol");
 
 contract("Election", function(accounts) {
